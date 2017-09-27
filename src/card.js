180ac5441b3f66e759212c833eeffb4b22e84d2f
Marika Ljungberg <marika@precisit.com>
2017-09-27 15:30:50 +0200

    Add callback to Card component to notify the Game when a card is clicked

--- src/card.js
+++ src/card.js
@@ -1,10 +1,23 @@
 import React from "react"
 import "./card.css";
 
-const Card = (props) => (
-    <div className="card">
-        <img src={props.src} alt={props.alt} />
-    </div>
-)
+
+class Card extends React.Component {
+
+    handleClick = () => {
+        this.props.onclick(this.props.id);
+    }
+
+    // Props become an object, so className="foo" becomes { className: "foo } on this.props,
+    // and onClick becomes { onClick: () => {} }
+    render() {
+        return (
+        <div className="card" onClick={this.handleClick}>
+            <img src={this.props.src} alt={this.props.alt}/>
+        </div>
+        )
+    }
+}
+
 
 export default Card
--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Card from "./card"
 import "./card.css";
+import shuffle from "shuffle-array"
 
 const photos = [
         "/images/appalachian.jpg",
@@ -30,33 +31,43 @@ class Game extends React.Component {
     }
 
     setupGame = () => {
-        const cards = photos.map((imgUrl) => (
-            {
-                src: imgUrl,
-                isFlipped: false
+        const duplicatedPhotos = photos.concat(photos)
+        const cards = duplicatedPhotos.map((imgUrl, i) => {
+            if (i > photos.length-1) {
+                i = i - photos.length
             }
-        ))
-        const duplicatedCards = cards.concat(cards)
-        console.log(duplicatedCards)
 
-        return duplicatedCards
+            return (
+                {
+                    src: imgUrl,
+                    alt: alts[i],
+                    isFlipped: false,
+                    id: Math.random()
+                }
+            )
+        })
+        const shuffledCards = shuffle(cards)
+        console.log(shuffledCards)
+
+        return cards
     }
+
+    handleCardClick = (cardId) => {
+        this.state.cards.forEach((card) => {
+            if (card.id === cardId) {
+                card.isFlipped = !card.isFlipped;
+            }
+        })
+    }
+
     
-    shuffle = (cards) => {
-        const shuffledCards = []
-        
-    }
-
-
-    renderCard = (card) => (
-        <Card src={card.src} alt={card.alt}/>
-    )
-
     render() {
         return (
         <div className="game">
             <h1>Ultra Memory Game</h1>
-            {this.state.cards.map(this.renderCard)}
+            {this.state.cards.map((card) => (
+                <Card src={card.src} alt={card.alt} id={card.id} onclick={this.handleCardClick}/>
+            ))}
         </div>
         )
     }
